ce6783ce865d5b14541f91bdbcaa4e79fa981276
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-19 11:34:52 -0400

    commit tree 74c8273720f73a602450c2154059767877c2d80e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(srcArray, callback = (arr) => {return arr}) {
+  return srcArray.map(element => callback(element));
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      console.log(`src: ${src} , callback: ${callback} , startingValue: ${startingValue} , total: ${total}`)
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      console.log(`src: ${src} , callback: ${callback} , total: ${total}`)
+      return total;
+    }
+  }
